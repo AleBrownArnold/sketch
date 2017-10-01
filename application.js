9c6c06499340863c48e2d75b8a58b22ce98284db
Alejandro Brown Arnold <charlie14_1939@hotmail.com>
2017-10-01 16:54:12 -0300

    Queda bien

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   let div = "<div class='interior'></div>";
   let div2 = "<div class='interior' id='ficogay'>Fico gay</div>";
+  let div3 = "<div class='interior' id='ficogay'>Toto gay</div>";
   let witdh = $(".exterior").css("width");
   let Size = 960;
   let squareSize = 960/10 + 'px';
@@ -8,29 +9,33 @@ $(document).ready(function(){
   $(".interior").css({"width": squareSize,"height": squareSize});
 
   for (let i=0; i<255;i++){
-    if (i === 20){
-      $(".exterior").append(div2)
-    }else{
-      $(".exterior").append(div)
-    };
+
+      let e = document.createElement("div");
+      e.className = "interior";
+
+      document.getElementsByClassName('exterior')[0].appendChild(e);
+
 
   };
 
-    $("button").on("click", function(){
+  $("button").on("click", function(){
     let amountSquares = prompt("Choose the amount of squares per side",8);
     let newSquareSize = 960/amountSquares + 'px';
     $(".exterior").empty()
 
-
     for (let i=0; i< (amountSquares * amountSquares);i++){
-      if (i === 20){
-        $(".exterior").append(div2)
-      }else{
-        $(".exterior").append(div)
-      };
-      $(".interior").css({"width": newSquareSize,"height": newSquareSize});
+      let e = document.createElement("div");
+      e.className = "interior";
+
+      document.getElementsByClassName('exterior')[0].appendChild(e);
+
     };
+    $(".interior").css({"width": newSquareSize,"height": newSquareSize});
+    $(".interior").hover(function(){
+      $(this).css("background-color","red")
+    });
   });
+
   $(".interior").hover(function(){
     $(this).css("background-color","red")
   });
